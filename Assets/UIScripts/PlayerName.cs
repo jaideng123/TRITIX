70a4b466446c98290598e2edfbfb109d52815550
Jaiden Gerig <jaideng123@yahoo.com>
2018-03-04 23:03:54 -0600

    UI is Mostly Done

---
+++ Assets/UIScripts/PlayerName.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour
+{
+
+    public Color defaultColor;
+    public Color activeColor;
+    private bool _active;
+    private Image image;
+    private Text nameText;
+    // Use this for initialization
+    void Start()
+    {
+        image = GetComponent<Image>();
+        nameText = GetComponentInChildren<Text>();
+        _active = false;
+        image.color = defaultColor;
+    }
+
+    public void SetActive(bool active)
+    {
+        _active = active;
+        if (_active)
+        {
+            image.color = activeColor;
+        }
+        if (!_active)
+        {
+            image.color = defaultColor;
+        }
+    }
+
+    public void SetName(string name)
+    {
+        nameText.text = name;
+    }
+
+
+
+}
